e355f5867d12f1b4cad4a0f2bffc3b105d40ae02
jamie <you@example.com>
2023-03-14 22:49:02 +0200

    Version 1.0

---
+++ App Preferences/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Configuration;
+
+
+namespace App_Preferences
+{
+	public partial class MainWindow : Window
+	{
+		Configuration AppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+		string[] Themes = new string[] { "Dark", "Light" };
+		string[] Languages = new string[] { "English", "Chinese" };
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			LanguageCombobox.ItemsSource = Languages;
+			ThemesCombobox.ItemsSource = Themes;
+
+
+			if (AppConfig.Sections["UISettings"] is null)
+			{
+				AppConfig.Sections.Add("UISettings", new UISettings());
+					
+			}
+
+
+			var UISettingSection = AppConfig.GetSection("UISettings");
+
+
+			this.DataContext = UISettingSection;
+
+
+
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			AppConfig.Save();
+		}
+	}
+}
---
+++ App Preferences/UISettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Preferences
+{
+	internal class UISettings:ConfigurationSection
+	{
+		[ConfigurationProperty("language",DefaultValue ="English")]
+		public string Language
+		{
+			get { return (string)this["language"]; }
+			set { this["language"] = value; }
+		}
+
+
+
+		[ConfigurationProperty("theme", DefaultValue = "Light")]
+		public string Theme
+		{
+			get { return (string)this["theme"]; }
+			set { this["theme"] = value; }
+		}
+
+		[ConfigurationProperty("currency", DefaultValue = "$")]
+		public string Currency
+		{
+			get { return (string)this["currency"]; }
+			set { this["currency"] = value; }
+		}
+
+		[ConfigurationProperty("fontsize", DefaultValue = 8)]
+		[IntegerValidator(MaxValue =100 , MinValue =5)]
+		public int FontSize
+		{
+			get { return (int)this["fontsize"]; }
+			set { this["fontsize"] = value; }
+		}
+
+
+	}
+}
